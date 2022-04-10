86abc7c3142a47db742145f1515877cb355d2d65
Rustamov Ixtiyor <90851461+muvozanat@users.noreply.github.com>
2022-04-10 17:55:37 +0500

    Update config.js

--- core/config.js
+++ core/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const  CONFIG =  {
-    UID: process.env.UID || 10043,
+    UID: process.env.UID,
     BOT_TOKEN: process.env.BOT_TOKEN ,
     API_TOKEN: process.env.API_TOKEN ,
     MONGO_LINK: process.env.MONGO_LINK ,
