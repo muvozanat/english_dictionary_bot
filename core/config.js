7fa5c1ea7e3f4929c69602f4bf84974b6b90d32f
Rustamov Ixtiyor <90851461+muvozanat@users.noreply.github.com>
2022-04-10 17:55:13 +0500

    error fixed

--- core/config.js
+++ core/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const  CONFIG =  {
     UID: process.env.UID || 10043,
-    BOT_TOKEN: process.env.BOT_TOKEN || '5254817857:AAHRsu9SoR04xqezh5szDbRcN24bDm9bPPo',
-    API_TOKEN: process.env.API_TOKEN || 'iSkDe4Q4X7mDNYLK',
-    MONGO_LINK: process.env.MONGO_LINK || 'mongodb+srv://DostonKarimov:DostonKarimov@cluster0.h5ied.mongodb.net/myFirstDatabase',
-    CHANNEL_ID: process.env.CHANNEL_ID || -1001669509893,
+    BOT_TOKEN: process.env.BOT_TOKEN ,
+    API_TOKEN: process.env.API_TOKEN ,
+    MONGO_LINK: process.env.MONGO_LINK ,
+    CHANNEL_ID: process.env.CHANNEL_ID,
 }
 
 module.exports = {CONFIG}
